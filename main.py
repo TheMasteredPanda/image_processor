6a9d189d2916a54638f677fbf5deff09c1334aec
Hattyot <Hattyot@gmail.com>
2020-06-02 15:32:37 +0300

    added gif support to distort

--- main.py
+++ main.py
@@ -58,15 +58,19 @@ def distort():
 
     with Image() as new_image:
         with Image(file=_img) as img:
+            if len(img.sequence) > 60:
+                return 'Gif has too many frames'
+
+            img.transform(resize='500x500')
+
             def transform_image(img):
-                img.transform(resize='500x500>')
                 img.liquid_rescale(width=int(img.width * 0.5), height=int(img.height * 0.5), delta_x=1)
                 img.liquid_rescale(width=int(img.width * 1.5), height=int(img.height * 1.5), delta_x=2)
-                img.transform(resize='500x500')
 
             if len(img.sequence) > 1:
-                transform_image(img.sequence[0])
-                new_image.sequence.append(img.sequence[0])
+                for frame in img.sequence:
+                    transform_image(frame)
+                    new_image.sequence.append(frame)
             else:
                 transform_image(img)
                 new_image.sequence.append(img)
